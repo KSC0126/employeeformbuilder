ed179c8298d4c256da87681c6c66a89332a24ba2
showrya <kollushowrya@gmail.com>
2019-01-05 12:23:13 -0500

    commit 01/05 12:23
    adding validators dynamicaly using reactive form methods such as setValidators,clearValidators and updateValueAndValidity

--- src/app/create-employee/create-employee.component.ts
+++ src/app/create-employee/create-employee.component.ts
@@ -15,6 +15,7 @@ export class CreateEmployeeComponent implements OnInit {
   formErrors = {
     'fullName': '',
     'email': '',
+    'phone': '',
     'skillName': '',
     'experienceInYears': '',
     'proficiency': ''
@@ -26,6 +27,9 @@ export class CreateEmployeeComponent implements OnInit {
   'maxlength':' Full name must be greater than 10 characters.',
 }, 'email': {
   'required': 'Email is required.'
+},
+ 'phone': {
+  'required': 'Phone is required.'
 },
 'skillName': {
   'required': 'Skill Name is required.',
@@ -43,7 +47,9 @@ export class CreateEmployeeComponent implements OnInit {
   ngOnInit() {
     this.employeeForm = this.fb.group({
       fullName:['',[Validators.required, Validators.minLength(2), Validators.maxLength(10)]],
+      contactPreference: ['email'],
       email: ['',Validators.required],
+      phone: [''],
       skills: this.fb.group({
         skillName:['',Validators.required],
         experienceInYears:['',Validators.required],
@@ -65,6 +71,9 @@ export class CreateEmployeeComponent implements OnInit {
     this.employeeForm.valueChanges.subscribe((data) =>{
       this.logValidationErrors(this.employeeForm);// inorder to call the logvalidationerrors function with any changes
     });    
+  this.employeeForm.get('contactPreference').valueChanges.subscribe((data: string)=>{
+  this.contactPreferenceClicked(data);}// using to check the value that changes in contact preference
+);
   }
   logValidationErrors(group: FormGroup= this.employeeForm):void{
     Object.keys(group.controls).forEach((key: string)=> {
@@ -85,6 +94,16 @@ export class CreateEmployeeComponent implements OnInit {
       }
     })  
 
+  }
+  contactPreferenceClicked( selectedValue: string){
+    const phoneControl =this.employeeForm.get('phone');
+    if(selectedValue === 'phone'){
+      phoneControl.setValidators(Validators.required);
+    }
+    else{
+      phoneControl.clearValidators();
+    }
+    phoneControl.updateValueAndValidity();
   }
   onSubmit():void{
     console.log(this.employeeForm.touched); 
